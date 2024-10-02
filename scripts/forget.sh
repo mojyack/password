5c8d1d36833d17ccf8b1a4bcd9fe9ac5fd54e0e2
mojyack <mojyack@gmail.com>
2024-10-02 13:35:14 +0900

    forget.sh: cleanup

--- scripts/forget.sh
+++ scripts/forget.sh
@@ -7,7 +7,7 @@ file=$1
 
 export FILTER_BRANCH_SQUELCH_WARNING=1
 git repack -Aq
-git branch -a | grep "remotes\/" | awk '{ print $1 }' | cut -f2 -d/ | while read -r r; do git remote rm "$r" 2>/dev/null; done
+git branch -a | grep "remotes/" | awk '{ print $1 }' | cut -f2 -d/ | while read -r r; do git remote rm "$r" 2>/dev/null; done
 git filter-branch --index-filter "git rm --cached --ignore-unmatch '$file'" --force -- --branches --tags
 rm -rf .git/refs/original/ .git/refs/remotes/ .git/*_HEAD .git/logs/
 (git for-each-ref --format="%(refname)" refs/original/ || echo :) | xargs --no-run-if-empty -n1 git update-ref -d
